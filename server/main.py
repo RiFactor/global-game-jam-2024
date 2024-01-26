e891cdced55d56de19c5acb3826999efde6b9458
fjebaker <fergusbkr@gmail.com>
2024-01-26 22:40:16 +0000

    server: dependencies and hello world
    Added basic `pdm` server setup.

---
+++ server/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/")
+async def get():
+    return HTMLResponse("Hello World")
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
