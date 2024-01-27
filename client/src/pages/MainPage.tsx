c188885b1d23d58204101af3dcee917a393cd4c1
JR-Mitchell <8377714+JR-Mitchell@users.noreply.github.com>
2024-01-27 22:10:53 +0000

    feat(frontend): removes test data and replaces with the state

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -14,26 +14,6 @@ const pixiApp = new Application({ resizeTo: window });
 
 // TODO test data, should be deleted
 const wordLengths = [2, 3, 4];
-const leftAnswer: KeyPress[] = [
-  { key: "i", userid: 1 },
-  { key: "m", userid: 1 },
-  { key: "t", userid: 2 },
-  { key: "h", userid: 1 },
-  { key: "e", userid: 2 },
-  { key: "b", userid: 1 },
-  { key: "s", userid: 2 },
-  { key: "o", userid: 1 },
-  { key: "s", userid: 2 }
-];
-const rightAnswer: KeyPress[] = [
-  { key: "m", userid: 1 },
-  { key: "i", userid: 1 },
-  { key: "h", userid: 1 },
-  { key: "t", userid: 1 }
-];
-
-// TODO this should take state that is currently in header; this needs refactoring in an upcoming PR
-const myUserId = 2;
 
 const MainPage = () => {
 
@@ -117,18 +97,18 @@ const MainPage = () => {
       <AppProvider value={pixiApp}>
         <FullScreenStage>
           <AnswerLengthIndicator
-            myUserId={myUserId}
+            myUserId={user_data?.userid}
             screenFraction={0.3}
             spacing={10}
             wordLengths={wordLengths}
-            currentAnswer={leftAnswer}
+            currentAnswer={ownAnswers}
             screenFractionOffset={0.1}
           />
           <AnswerLengthIndicator
             screenFraction={0.3}
             spacing={10}
             wordLengths={wordLengths}
-            currentAnswer={rightAnswer}
+            currentAnswer={enemyAnswers}
             screenFractionOffset={0.6}
           />
         </FullScreenStage>
