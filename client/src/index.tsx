09339e4d4021902222bb52de49532063c6771a73
Ri <ri@3sidedcube.com>
2024-01-27 00:55:50 +0000

    Routing, folder structure, component, error page

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { RouterProvider } from "react-router-dom";
+import router from "./pages/routes";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ client/src/pages/MainPage.tsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+import NavBar from "../components/NavBar";
+
+const MainPage = () => {
+  return (
+    <>
+      <Header />
+      <NavBar />
+      <div>Main Page</div>;
+    </>
+  );
+};
+
+export default MainPage;
