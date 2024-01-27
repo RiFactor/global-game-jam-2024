9c4610d06749eb5df30f72d5c0d6ae11e5eb374b
JR-Mitchell <8377714+JR-Mitchell@users.noreply.github.com>
2024-01-27 20:16:35 +0000

    feat(frontend): adds an optional character prop to SingleLetterIndicator.tsx

--- client/src/components/SingleLetterIndicator.tsx
+++ client/src/components/SingleLetterIndicator.tsx
@@ -1,7 +1,7 @@
 import { useCallback } from "react";
-import { Graphics, _ReactPixi } from "@pixi/react";
+import { Container, Graphics, Text, _ReactPixi } from "@pixi/react";
 
-function SingleLetterIndicator(props: {width: number, x: number, y: number}) {
+function SingleLetterIndicator(props: {width: number, x: number, y: number, character?: string}) {
     const draw = useCallback((g: _ReactPixi.IGraphics) => {
         g.clear!();
         g.beginFill!(0xffffff, 1);
@@ -9,7 +9,12 @@ function SingleLetterIndicator(props: {width: number, x: number, y: number}) {
         g.endFill!();
     }, [])
 
-    return <Graphics draw={draw} />
+    return <>
+        <Graphics draw={draw} />
+        {props.character ? <Container x={props.x} y={props.y} height={props.width} width={props.width}>
+            <Text text={props.character} anchor={0.5} x={props.width / 2} y={props.width / 2} />
+        </Container> : undefined}
+    </>
 }
 
 export default SingleLetterIndicator;
