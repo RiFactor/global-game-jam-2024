95ede74a2e2960b60ad4da1bb4e239d720a4acb4
Ri <ri@3sidedcube.com>
2024-01-27 12:22:52 +0000

    Routes to /games

--- client/src/pages/routes.tsx
+++ client/src/pages/routes.tsx
@@ -1,18 +1,19 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 import MainPage from "./MainPage";
 import ErrorPage from "./ErrorPage";
 import MainLayout from "../layouts/MainLayout";
 
 const router = createBrowserRouter([
+  { path: "", element: <Navigate to="/games" /> },
   {
-    path: "/",
+    path: "/games",
     element: <MainLayout />,
     errorElement: <ErrorPage />,
     children: [
-      { index: true, element: <MainPage /> },
+      { index: true, element: <MainPage /> }
       //   { path: "games/:slug", element: <GameDetailPage /> },
-    ],
-  },
+    ]
+  }
 ]);
 
 export default router;
