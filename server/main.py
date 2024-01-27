009ed47f6cb8afeda39229d30a494456659cf163
fjebaker <fergusbkr@gmail.com>
2024-01-27 11:41:56 +0000

    server: refactor
    Moving comms loop to be owned by user connection.

--- server/main.py
+++ server/main.py
@@ -4,58 +4,13 @@ from fastapi.staticfiles import StaticFiles
 
 import logging
 
+from server.connections import ConnectionManager
+
 # prepend uvicorn so it all uses the same handler
 logger = logging.getLogger("uvicorn." + __name__)
 logger.setLevel(logging.DEBUG)
 logger.info("Server starting...")
 
-
-class UserData:
-    def __init__(self, manager: "ConnectionManager", socket: WebSocket) -> None:
-        self.manager = manager
-        self.identity = hash(socket)
-        self.socket = socket
-        self.name = ""
-        self.score = 0
-
-    async def receive(self) -> str:
-        return await self.socket.receive_text()
-
-    async def send_message(self, message: str) -> None:
-
-        logger.debug("Sending message to %s (%s)", self.identity, self.name)
-
-        await self.socket.send_text(message)
-
-    async def send_broadcast(self, message: str) -> None:
-        """Broadcast to all other users"""
-
-        logger.debug("Broadcasting to all from %s (%s)", self.identity, self.name)
-
-        for _id, user in self.manager.usermap.items():
-            if _id == self.identity:
-                continue
-            await user.socket.send_text(message)
-
-
-class ConnectionManager:
-    def __init__(self):
-        self.usermap: dict[int, UserData] = {}
-
-    async def connect(self, websocket: WebSocket) -> UserData:
-        await websocket.accept()
-
-        data = UserData(self, websocket)
-        self.usermap[data.identity] = data
-        return data
-
-    def disconnect(self, user: UserData) -> None:
-        del self.usermap[user.identity]
-
-    async def broadcast(self, message: str) -> None:
-        """Broadcast to all users"""
-        for _, user in self.usermap.items():
-            await user.socket.send_text(message)
 
 
 app = FastAPI()
@@ -74,9 +29,8 @@ async def websocket_endpoint(websocket: WebSocket):
 
     try:
         while True:
-            data = await user.receive()
-            await user.send_message(f"You sent: {data}")
-            await user.send_broadcast(f"{user.identity} sends: {data}")
+            await user.comm_loop()
     except WebSocketDisconnect:
+        logger.info("%s disconnected", user.identity)
         manager.disconnect(user)
-        # await manager.broadcast(f"Client {user.identity} left the chat")
+
