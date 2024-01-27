af3b6a7890549f2c86a39fb151548e52f5fa542f
Shellywell123 <benjamin@shellswell.com>
2024-01-27 15:33:05 +0000

    upgrades people upgrades

--- server/parse-prompts.py
+++ server/parse-prompts.py
@@ -2,20 +2,23 @@ def ParsePrompts(filename) :
     """
     create a dictionary of difficulty to lists of prompts 
     """
-    prompts = {}
+    promptLookups = {}
     f = open(filename, 'r')
     for line in f:
-        w, p= line.strip().split('-')
-        prompt = {w.strip(), p.strip()}
-        difficulty = len(w.strip())
-        if difficulty not in prompts:
-            prompts[difficulty] = []
-        prompts[difficulty].append(prompt)
-    return prompts
+        data = line.strip().split(',')
+        promptLookup = {
+            "word":data[0].strip(),
+            "prompts": [x.strip() for x in data[1:]]
+            }
+        difficulty = len(data[0].strip())
+        if difficulty not in promptLookups:
+            promptLookups[difficulty] = []
+        promptLookups[difficulty].append(promptLookup)
+    return promptLookups
 
-def GetRandomPromptOfDifficulty(prompts,difficulty):
+def GetRandomPromptOfDifficulty(promptLookups,difficulty):
     import random
-    return random.choice(list(prompts[difficulty]))
+    return random.choice(list(promptLookups[difficulty]))
 
-# example retrieval of a random prompt of difficulty 3
-print(f"Example of difficulty 3: {GetRandomPromptOfDifficulty(ParsePrompts('../assets/prompts.txt'), 3)}")
+# example retrieval of a random word and prompt/s of difficulty 3
+print(f"Example of difficulty 3: {GetRandomPromptOfDifficulty(ParsePrompts('../assets/prompts.csv'), 3)}")
