47c5ee85806a54963ff91f069c2b3f498451b980
JR-Mitchell <8377714+JR-Mitchell@users.noreply.github.com>
2024-01-27 12:00:03 +0000

    feat: uses the new FullScreenStage component in MainPage.tsx

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -1,17 +1,24 @@
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
-import { Stage, Container, Sprite, Text } from '@pixi/react';
+import FullScreenStage from "../components/FullScreenStage";
+import { Container, Text, AppProvider } from '@pixi/react';
+import { Application } from 'pixi.js';
+
+// TODO: is there a better way to do this than just declaring here?
+const pixiApp = new Application({resizeTo: window})
 
 const MainPage = () => {
   return (
     <>
       <Header />
       <NavBar />
-      <Stage options={{ backgroundColor: 0xeef1f5 }}>
-        <Container x={400} y={330}>
-          <Text text="Hello World" anchor={{ x: 0.5, y: 0.5 }} />
-        </Container>
-      </Stage>
+      <AppProvider value={pixiApp}>
+        <FullScreenStage>
+          <Container x={400} y={330}>
+            <Text text="Hello World" anchor={{ x: 0.5, y: 0.5 }} />
+          </Container>
+        </FullScreenStage>
+      </AppProvider>
     </>
   );
 };
