7fcd579a7c61090ec153c8a07fd48f091c4b867b
fjebaker <fergusbkr@gmail.com>
2024-01-27 16:03:11 +0000

    server: json event communication

--- server/connections.py
+++ server/connections.py
@@ -1,9 +1,27 @@
+from typing import Any
 from fastapi import WebSocket
+
 import logging
+import json
+
+from dataclasses import dataclass
+
+@dataclass
+class Event:
+    event: str
+    data: dict
+
+    @staticmethod
+    def from_json(text: str) -> "Event":
+        data = json.loads(text)
+        return Event(data["event"], data["data"])
+
+    def to_json(self) -> str:
+        event = dict(event=self.event, data=self.data)
+        return json.dumps(event)
 
 # prepend uvicorn so it all uses the same handler
 logger = logging.getLogger("uvicorn." + __name__)
-
 
 class UserConnection:
     def __init__(self, manager: "ConnectionManager", socket: WebSocket) -> None:
@@ -12,20 +30,23 @@ class UserConnection:
         self.socket = socket
         self.name = ""
 
-    async def receive(self) -> str:
-        return await self.socket.receive_text()
+    async def receive_event(self) -> Event:
+        text = await self.socket.receive_text()
+        return Event.from_json(text)
+
+
+    async def broadcast(self, event: Event) -> None:
+        await self.send_broadcast(event.to_json())
 
     async def send_message(self, message: str) -> None:
 
         logger.debug("Sending message to %s (%s)", self.identity, self.name)
-
         await self.socket.send_text(message)
 
     async def send_broadcast(self, message: str) -> None:
         """Broadcast to all other users"""
 
         logger.debug("Broadcasting to all from %s (%s)", self.identity, self.name)
-
         for _id, user in self.manager.usermap.items():
             if _id == self.identity:
                 continue
@@ -33,10 +54,9 @@ class UserConnection:
 
     async def comm_loop(self) -> None:
         """Main communications loop"""
-        data = await self.receive()
-        await self.send_message(f"You sent: {data}")
-        await self.send_broadcast(f"{self.identity} sends: {data}")
-
+        event = await self.receive_event()
+        logger.info("%s event from %s", event.event, self.identity)
+        await self.broadcast(event)
 
 class ConnectionManager:
     def __init__(self):
