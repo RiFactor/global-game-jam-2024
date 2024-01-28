2bf27cf378ed5ed18e3055e3e0de0245c1379e97
JR-Mitchell <8377714+JR-Mitchell@users.noreply.github.com>
2024-01-28 14:36:33 +0000

    feat(frontend): attempts to play audio in index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { RouterProvider } from "react-router-dom";
 import router from "./pages/routes";
+import { sound } from "@pixi/sound";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -13,6 +14,18 @@ root.render(
     <RouterProvider router={router} />
   </>
 );
+
+const loop = sound.add("loop", {
+  url: "assets/audio/loop.mp3"
+});
+sound.add("intro", {
+  url: "assets/audio/intro.mp3",
+  autoPlay: true,
+  complete: () => {
+    loop.play();
+  }
+});
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
